6e689afbe612c9151581336f7061c649ee7d2711
fernando <fernandoporras4444@yahoo.com>
2021-11-20 11:50:55 -0600

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -5,44 +5,6 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
 
-# Import the datetime class from the datetime module.
-# import datetime as dt
-# # Use the now() attribute on the datetime class to get the present time.
-# now = dt.datetime.now()
-# # Print the present time
-# print("THe time right now is ", now)
-
-# # Assign a variable for the file to load and the path
-# file_to_load = "Resources\election_results.csv"
-
-# # Open the election results and read the file
-# election_data = open(file_to_load, "r")
-
-# # To do: perform analysis
-# print(election_data)
-
-# # Close the file
-# election_data.close()
-
-# # Create a filename variable to a direct or indirect path to the file
-# file_to_save = "analysis\election_analysis.txt"
-
-# # Use the open statement to open the file as a text file
-# outfile = open(file_to_save, "w")
-
-# # Write some data to the file
-# #outfile.write("hello world")
-# # Write three counties to the file
-# #outfile.write("Arapahoe, ")
-# #utfile.write("Denver, ")
-# #outfile.write("Jefferson")
-# #outfile.write("Arapahoe, Denver, Jefferson")
-
-# outfile.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-
-# # Close the file
-# outfile.close()
-
 #Add our dependencies
 import csv
 import os
